b03b00289fa3f529ec2971baeaf63360c4af2c1d
Denis <76144861+dantedenis@users.noreply.github.com>
2022-05-24 14:02:47 +0300

    Update ApiService.go

--- service/ApiService.go
+++ service/ApiService.go
@@ -21,6 +21,27 @@ func (a ApiService) Authorizations(ctx context.Context, authorizationRequest vo.
 }
 
 func (a ApiService) Capture(ctx context.Context, captureRequest vo.CaptureRequest) (vo.TransactionResponse, error) {
-	//TODO implement me
-	panic("implement me")
+	ctx, cancelCtx = context.WithTimeout(ctx, 3 * time.Second)
+	respChan, errChan := make(chan *Response), make(chan error)
+	request := map[string]interface{} /// -> TODO: roadmap
+	
+	go func(){
+		bytes, err := json.Marshal(message)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		resp, err := http.POST('https://gateway.bepaid.by/transactions/captures','application/json', bytes.NewBuffer(bytes))
+		if err != nil {
+			errChan <- err
+		}
+		respChan <- resp
+	}()
+	select {
+	case <- cts.Done():
+		cancelCtx()
+	case <- respChan:
+		
+	case <- errChan:
+		
+	}
 }
