ec20272b86a18017190e4b13506b45863226afda
Denis <76144861+dantedenis@users.noreply.github.com>
2022-05-25 10:55:20 +0300

    Update CaptureRequest.go

--- service/vo/CaptureRequest.go
+++ service/vo/CaptureRequest.go
@@ -1,8 +1,18 @@
 package vo
 
+const (
+	URLRequest = "https://gateway.bepaid.by/transactions/captures"	
+)
+
 type CaptureRequest struct {
+	Amount int
+	Parent_uid string
+	Status string
+	Message string
+	Uid	string
+	Gateway_id int
 }
 
-func NewCaptureRequest() *CaptureRequest {
-	return &CaptureRequest{}
+func NewCaptureRequest(amount int, parent_uid string) *CaptureRequest {
+	return &CaptureRequest{Amount: amount, Parent_uid: parent_uid}
 }
