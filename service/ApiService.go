5e49aeb6c11af7015ff66e2dc74af2d8fa9a1e50
tim <timaz98@ya.ru>
2022-05-27 20:07:48 +0500

    remove ApiService implementation and tests

--- service/ApiService.go
+++ service/ApiService.go
@@ -4,7 +4,6 @@ import (
 	"bepaid-sdk/api/contracts"
 	"bepaid-sdk/service/vo"
 	"context"
-	"encoding/json"
 )
 
 type ApiService struct {
@@ -15,34 +14,13 @@ func NewApiService(api contracts.Api) *ApiService {
 	return &ApiService{api: api}
 }
 
-func (c *ApiService) Authorizations(ctx context.Context, authorizationRequest vo.AuthorizationRequest) (*vo.TransactionResponse, error) {
-	resp, err := c.api.Authorizations(ctx, authorizationRequest)
-	if err != nil {
-		return nil, err
-	}
+func (a ApiService) Authorizations(ctx context.Context, authorizationRequest vo.AuthorizationRequest) (vo.TransactionResponse, error) {
+	//TODO implement me
+	panic("implement me")
 
-	defer resp.Body.Close()
-
-	var tr vo.TransactionResponse
-	if err = json.NewDecoder(resp.Body).Decode(&tr); err != nil {
-		return nil, err
-	}
-
-	return &tr, err
 }
 
-func (c *ApiService) Capture(ctx context.Context, captureRequest vo.CaptureRequest) (*vo.TransactionResponse, error) {
-	resp, err := c.api.Captures(ctx, captureRequest)
-	if err != nil {
-		return nil, err
-	}
-
-	defer resp.Body.Close()
-
-	var tr vo.TransactionResponse
-	if err = json.NewDecoder(resp.Body).Decode(&tr); err != nil {
-		return nil, err
-	}
-
-	return &tr, err
+func (a ApiService) Capture(ctx context.Context, captureRequest vo.CaptureRequest) (vo.TransactionResponse, error) {
+	//TODO implement me
+	panic("implement me")
 }
