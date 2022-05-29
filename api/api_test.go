f2abf2c55d28567180085599c468276f334d3a06
tim <timaz98@ya.ru>
2022-05-29 18:53:20 +0500

    refactor and add tests to api_test.go.

--- api/api_test.go
+++ api/api_test.go
@@ -14,7 +14,16 @@ type C = vo.CaptureRequest
 type V = vo.VoidRequest
 type R = vo.RefundRequest
 
-var ch = make(chan io.ReadCloser, 1)
+var (
+	// All tests use this channel. So, no parallel testing.
+	ch = make(chan io.ReadCloser, 1)
+
+	// testingClient sends Request.Body to ch.
+	testingClient = &http.Client{Transport: customRoundTripper{}}
+
+	// Default Api used by all tests.
+	api = Api{client: testingClient}
+)
 
 type customRoundTripper struct{}
 
@@ -23,197 +32,141 @@ func (customRoundTripper) RoundTrip(request *http.Request) (*http.Response, erro
 	return nil, nil
 }
 
-//---------MarshalRequest---------
 func TestApi_PaymentsMarshalRequest(t *testing.T) {
-
-	api := NewApi(
-		&http.Client{Transport: customRoundTripper{}},
-		"",
-		"",
-		"",
-	)
 
 	tests := []struct {
 		name string
-		data P
+		req  P
 		er   string
 	}{
-		{"test1", P{}, `{"request":{"amount":0,"currency":"","description":"","tracking_id":"","test":false,"credit_card":{"number":"","verification_value":"","holder":"","exp_month":"","exp_year":"","skip_three_d_secure_verification":false}}}`},
+		{"defaultValue", P{}, `{"request":{"amount":0,"currency":"","description":"","tracking_id":"","test":false,"credit_card":{"number":"","verification_value":"","holder":"","exp_month":"","exp_year":"","skip_three_d_secure_verification":false}}}`},
+		{"requestConstructor", *vo.NewPaymentRequest(int64(1), "rub", "rub_1", "id1", true, *vo.NewCreditCard("5555", "123", "tim", "05", "2024")), `{"request":{"amount":1,"currency":"rub","description":"rub_1","tracking_id":"id1","test":true,"credit_card":{"number":"5555","verification_value":"123","holder":"tim","exp_month":"05","exp_year":"2024","skip_three_d_secure_verification":false}}}`},
 	}
 
 	for _, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
-			// ignore response and err
-			go api.Payments(context.TODO(), tc.data)
-
-			body := <-ch
-			defer body.Close()
-
-			b, err := io.ReadAll(body)
-
-			if err != nil {
-				fatalfExpectedActual(t, "ReadAll returned not nil value", nil, err)
-			}
-
-			if string(b) != tc.er {
-				fatalfExpectedActual(t, "Strings aren't equal", tc.er, string(b))
-			}
+			testMarshallRequest(
+				t,
+				tc.er,
+				func() (*http.Response, error) {
+					return api.Payments(context.TODO(), tc.req)
+				})
 		})
 	}
 }
 
 func TestApi_AuthorizationsMarshalRequest(t *testing.T) {
 
-	api := NewApi(
-		&http.Client{Transport: customRoundTripper{}},
-		"",
-		"",
-		"",
-	)
-
 	tests := []struct {
 		name string
-		data A
+		req  A
 		er   string
 	}{
-		{"test1", A{}, `{"request":{"amount":0,"currency":"","description":"","tracking_id":"","test":false,"credit_card":{"number":"","verification_value":"","holder":"","exp_month":"","exp_year":"","skip_three_d_secure_verification":false}}}`},
+		{"defaultValue", A{}, `{"request":{"amount":0,"currency":"","description":"","tracking_id":"","test":false,"credit_card":{"number":"","verification_value":"","holder":"","exp_month":"","exp_year":"","skip_three_d_secure_verification":false}}}`},
+		{"requestConstructor", *vo.NewAuthorizationRequest(int64(1), "rub", "rub_1", "id1", true, *vo.NewCreditCard("5555", "123", "tim", "05", "2024")), `{"request":{"amount":1,"currency":"rub","description":"rub_1","tracking_id":"id1","test":true,"credit_card":{"number":"5555","verification_value":"123","holder":"tim","exp_month":"05","exp_year":"2024","skip_three_d_secure_verification":false}}}`},
 	}
 
 	for _, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
-			// ignore response and err
-			go api.Authorizations(context.TODO(), tc.data)
-
-			body := <-ch
-			defer body.Close()
-
-			b, err := io.ReadAll(body)
-
-			if err != nil {
-				t.Fatalf("ReadAll returned not nil value: \nER: %v,\nAR: %v", nil, err)
-			}
-
-			if string(b) != tc.er {
-				t.Fatalf("wrong value: \nER: %v,\nAR: %v", tc.er, string(b))
-			}
+			testMarshallRequest(
+				t,
+				tc.er,
+				func() (*http.Response, error) {
+					return api.Authorizations(context.TODO(), tc.req)
+				})
 		})
 	}
 }
 
 func TestApi_CapturesMarshalRequest(t *testing.T) {
 
-	api := NewApi(
-		&http.Client{Transport: customRoundTripper{}},
-		"",
-		"",
-		"",
-	)
-
 	tests := []struct {
 		name string
-		data C
+		req  vo.CaptureRequest
 		er   string
 	}{
-		{"test1", C{}, `{"request":{"parent_uid":"","amount":0}}`},
+		{"defaultValue", C{}, `{"request":{"parent_uid":"","amount":0}}`},
+		{"requestConstructor", *vo.NewCaptureRequest("id123", int64(63)), `{"request":{"parent_uid":"id123","amount":63}}`},
 	}
 
 	for _, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
-			// ignore response and err
-			go api.Captures(context.TODO(), tc.data)
-
-			body := <-ch
-			defer body.Close()
-
-			b, err := io.ReadAll(body)
-
-			if err != nil {
-				t.Fatalf("ReadAll returned not nil value: \nER: %v,\n AR: %v", nil, err)
-			}
-
-			if string(b) != tc.er {
-				t.Fatalf("wrong value: \nER: %v,\n AR: %v", tc.er, string(b))
-			}
+			testMarshallRequest(
+				t,
+				tc.er,
+				func() (*http.Response, error) {
+					return api.Captures(context.TODO(), tc.req)
+				})
 		})
 	}
 }
 
 func TestApi_VoidsMarshalRequest(t *testing.T) {
 
-	api := NewApi(
-		&http.Client{Transport: customRoundTripper{}},
-		"",
-		"",
-		"",
-	)
-
 	tests := []struct {
 		name string
-		data V
+		req  V
 		er   string
 	}{
-		{"test1", V{}, `{"request":{"parent_uid":"","amount":0}}`},
+		{"defaultValue", V{}, `{"request":{"parent_uid":"","amount":0}}`},
+		{"requestConstructor", *vo.NewVoidRequest("id123", int64(63)), `{"request":{"parent_uid":"id123","amount":63}}`},
 	}
 
 	for _, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
-			// ignore response and err
-			go api.Voids(context.TODO(), tc.data)
-
-			body := <-ch
-			defer body.Close()
-
-			b, err := io.ReadAll(body)
-
-			if err != nil {
-				t.Fatalf("ReadAll returned not nil value: \nER: %v,\n AR: %v", nil, err)
-			}
-
-			if string(b) != tc.er {
-				t.Fatalf("wrong value: \nER: %v,\n AR: %v", tc.er, string(b))
-			}
+			testMarshallRequest(
+				t,
+				tc.er,
+				func() (*http.Response, error) {
+					return api.Voids(context.TODO(), tc.req)
+				})
 		})
 	}
 }
 
 func TestApi_RefundsMarshalRequest(t *testing.T) {
 
-	api := NewApi(
-		&http.Client{Transport: customRoundTripper{}},
-		"",
-		"",
-		"",
-	)
-
 	tests := []struct {
 		name string
-		data R
+		req  R
 		er   string
 	}{
-		{"test1", R{}, `{"request":{"parent_uid":"","amount":0,"reason":""}}`},
+		{"defaultValue", R{}, `{"request":{"parent_uid":"","amount":0,"reason":""}}`},
+		{"requestConstructor", *vo.NewRefundRequest("id123", int64(63), "reason"), `{"request":{"parent_uid":"id123","amount":63,"reason":"reason"}}`},
 	}
 
 	for _, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
-			// ignore response and err
-			go api.Refunds(context.TODO(), tc.data)
-
-			body := <-ch
-			defer body.Close()
-
-			b, err := io.ReadAll(body)
-
-			if err != nil {
-				fatalfExpectedActual(t, "ReadAll returned not nil value", nil, err)
-			}
-
-			if string(b) != tc.er {
-				fatalfExpectedActual(t, "String are not equal", tc.er, string(b))
-			}
+			testMarshallRequest(
+				t,
+				tc.er,
+				func() (*http.Response, error) {
+					return api.Refunds(context.TODO(), tc.req)
+				})
 		})
 	}
 }
 
-func fatalfExpectedActual(t *testing.T, msg string, er, ar interface{}) {
+//---------MarshalRequest---------
+
+func testMarshallRequest(t *testing.T, er string, startRequest func() (*http.Response, error)) {
+	// ignore response and error
+	go startRequest()
+
+	body := <-ch
+	defer body.Close()
+
+	b, err := io.ReadAll(body)
+
+	if err != nil {
+		fatalfWithExpectedActual(t, "ReadAll returned not nil value", nil, err)
+	}
+
+	if string(b) != er {
+		fatalfWithExpectedActual(t, "Strings aren't equal", er, string(b))
+	}
+}
+
+func fatalfWithExpectedActual(t *testing.T, msg string, er, ar interface{}) {
 	t.Fatalf("%s:\nER: %v,\nAR: %v", msg, er, ar)
 }
